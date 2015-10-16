79be3477529812f70218aa16d99e1aab955a8838
James Cui <james.q.cui@gmail.com>
2015-10-16 17:01:37 -0400

    Updated config.py to have a base and test configuration

--- config.py
+++ config.py
@@ -2,8 +2,15 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = False
-TESTING = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'FOS.db')
-SECRET_KEY = '\xb4\xea\x963\x1d\xeb\x7f\x87S\xfe*\x94\x11\x1c\xd7\x9d\xc8\xdeq|u\xc7a\xb7'
 
+class BaseConfiguration(object):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'FOS.db')
+    SECRET_KEY = '\xb4\xea\x963\x1d\xeb\x7f\x87S\xfe*\x94\x11\x1c\xd7\x9d\xc8\xdeq|u\xc7a\xb7'
+
+
+class TestConfiguration(BaseConfiguration):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'FOS_test.db')
