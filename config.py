0a57e2074c036fa584345ebbda7146ad8af4a5f1
James Cui <james.q.cui@gmail.com>
2015-10-16 11:37:58 -0400

    Added a secret_key for secure communications

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = False
 TESTING = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'FOS.db')
-
+SECRET_KEY = '\xb4\xea\x963\x1d\xeb\x7f\x87S\xfe*\x94\x11\x1c\xd7\x9d\xc8\xdeq|u\xc7a\xb7'
 
