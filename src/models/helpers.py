e4daabcd95fef928d5d5360ccd6a01c8cf227789
James Cui <james.q.cui@gmail.com>
2015-10-19 15:39:14 -0400

    Moved models.py into a separate module. Added helpers.py for models

---
+++ src/models/helpers.py
@@ -0,0 +1,67 @@
+from . import db
+from collections import OrderedDict
+
+
+"""
+Serializer that serializes model data into JSON
+"""
+
+
+class Serializer(object):
+    def serialize(self, **kwargs):
+        import unicodedata
+        result, ignore_keys = OrderedDict(), kwargs.get('ignore', set())
+        keys = [k for k in self.__mapper__.c.keys() if k not in ignore_keys]
+        for key in keys:
+            result[key] = getattr(self, key)
+            if isinstance(result[key], unicode):
+                # convert unicode string to regular ascii string
+                result[key] = unicodedata.normalize('NFKD', result[key]).encode('ascii', 'ignore')
+            elif hasattr(result[key], 'isoformat'):
+                # avoid datetime not serializable errors
+                result[key] = result[key].isoformat()
+        return result
+
+
+"""
+Custom CRUD Mixin to use with models
+"""
+
+
+class CRUDMixin(object):
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    @classmethod
+    def get_by_id(cls, id):
+        if any(
+                (isinstance(id, basestring) and id.isdigit(),
+                 isinstance(id, (int, float))),
+        ):
+            return cls.query.get(int(id))
+        return None
+
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.iteritems():
+            setattr(self, attr, value)
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+        return self
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        return commit and db.session.commit()
+
+
+class ModelMixins(CRUDMixin, Serializer):
+    pass
--- src/models/models.py
+++ src/models/models.py
@@ -1,53 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-'''
-Custom CRUD Mixin to use with models
-'''
+from . import db
+from helpers import ModelMixins
+import datetime
+import math
+from flask_login import UserMixin as UserStatusMixin
 
 
-class CRUDMixin(object):
-    __table_args__ = {'extend_existing': True}
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-    @classmethod
-    def get_by_id(cls, id):
-        if any(
-                (isinstance(id, basestring) and id.isdigit(),
-                 isinstance(id, (int, float))),
-        ):
-            return cls.query.get(int(id))
-        return None
-
-    @classmethod
-    def create(cls, **kwargs):
-        instance = cls(**kwargs)
-        return instance.save()
-
-    def update(self, commit=True, **kwargs):
-        for attr, value in kwargs.iteritems():
-            setattr(self, attr, value)
-        return commit and self.save() or self
-
-    def save(self, commit=True):
-        db.session.add(self)
-        if commit:
-            db.session.commit()
-        return self
-
-    def delete(self, commit=True):
-        db.session.delete(self)
-        return commit and db.session.commit()
-
-
-'''
-Models
-'''
-
-
-class User(db.Model, CRUDMixin):
+class User(db.Model, ModelMixins, UserStatusMixin):
     """
     A user owns a membership
     A user is created together with a membership
@@ -56,7 +14,7 @@ class User(db.Model, CRUDMixin):
     __tablename__ = 'user'
 
     account_id = db.Column(db.String(255), unique=True, nullable=False)
-    secret_key = db.Column(db.String(255))
+    secret_key = db.Column(db.String(32))
     email = db.Column(db.String(255), unique=True, nullable=False)
     membership = db.relationship('Membership', uselist=False, backref='user', cascade='all, delete-orphan')
 
@@ -64,13 +22,10 @@ class User(db.Model, CRUDMixin):
         self.email = kwargs.get('email')
         self.account_id = kwargs.get('account_id')
         self.secret_key = kwargs.get('secret_key')
-
-    def add_membership(self):
-        self.membership = Membership(user_id=self.id).save()
-        return self.membership
+        self.membership = Membership(user_id=self.id)
 
 
-class Membership(db.Model, CRUDMixin):
+class Membership(db.Model, ModelMixins):
     """
     A membership belongs to a user
     A membership is associated with many purchases
@@ -83,28 +38,38 @@ class Membership(db.Model, CRUDMixin):
     credits = db.Column(db.Integer, default=0)
     purchases = db.relationship('Purchase', backref='membership', cascade='all, delete-orphan')
 
-
-    _points_per_dollar = 10
-    _redeemable_threshold = 100  # redeem at least 100 pts
-
     def __init__(self, **kwargs):
         self.user_id = kwargs.get('user_id')
         self.points = kwargs.get('points')
         self.credits = kwargs.get('credits')
 
+    @classmethod
+    def price_to_points(cls, price):
+        """
+        Policy to decide price X converts into Y points
+        Current policy: 1$ = 1 pt
+        """
+        return int(math.floor(price))
 
-class Purchase(db.Model, CRUDMixin):
+
+class Purchase(db.Model, ModelMixins):
     """
     A purchase is associated with a membership
     A purchase is deleted when its associated membership is deleted
     """
     __tablename__ = 'purchase'
 
-    price = db.Column(db.Float)
-    date = db.Column(db.DateTime)
+    title = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    points = db.Column(db.Integer, default=0)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     membership_id = db.Column(db.Integer, db.ForeignKey('membership.id'))
 
     def __init__(self, **kwargs):
+        self.title = kwargs.get('title')
         self.price = kwargs.get('price')
-        self.date = kwargs.get('date', None)
+        self.points = Membership.price_to_points(self.price)
+        self.date = kwargs.get('date')
         self.membership_id = kwargs.get('membership_id')
+
+
