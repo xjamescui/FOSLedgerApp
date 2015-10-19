e76a890029fd8a19a1118faddf3ddba768f76b44
James Cui <james.q.cui@gmail.com>
2015-10-19 15:43:10 -0400

    Housekeeping stuff: bower.json, gitignores and __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from flask_migrate import Migrate
+
 from src.models import db
 from src.assets_loader import assets
+from src.api import api
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
+app.config.from_object('config.BaseConfiguration')
 app.config.from_pyfile('config.py', silent=True)
 
+api.init_app(app)
 assets.init_app(app)
 db.init_app(app)
-migrate = Migrate(app, db)
-
 
 import src.controllers
